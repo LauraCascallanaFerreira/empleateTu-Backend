26065ed3d4f66ed5b7dbd259b243bb81e339055f
LauraCascallana <lauracascallanaferreira@gmail.com>
2025-02-13 09:31:29 +0100

    ya no se que hice

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import helmet from 'helmet'
 import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import morgan from 'morgan'
 
 
 
@@ -23,6 +24,7 @@ app.use(cors({
 app.use(express.json())
 app.use(helmet())
 app.use(compression())
+app.use(morgan('tiny'))
 
 
 const limiter = rateLimit({
